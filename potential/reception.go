7538d617fa6b1ec627ed8f75abf02fed34f2a2c6
Jeff H. Parrish <jeffhparrish@gmail.com>
2016-11-13 06:38:44 -0800

    first hack of a receptor interface

---
+++ potential/reception.go
@@ -0,0 +1,29 @@
+package potential
+
+/*
+ReceptorLayer is a flat network of Receptors.
+*/
+type ReceptorLayer struct {
+	Receptors []Receptor
+}
+
+/*
+Receptor receives external output and is connected to a cell. It has a corresponding output cell.
+When this receptor fires, it always fires its cell.
+
+When building a network, the developer should inherit Receptor and make `Value` their own type
+(instead of leaving it as `interface{}`).
+*/
+type Receptor struct {
+	/*
+	   Value should be a unique piece of information which this receptor represents. In the case
+	   of a human eye, *cones* are receptors for location and color. So the `Value` might include
+	   a struct with x and y location coordinates, the kind of cone (red, green, or blue). The
+	   combinations of these values should be unique.
+	*/
+	Value interface{}
+	/*
+	   The normal network cell that this receptor fires.
+	*/
+	CellFiree *Cell
+}
