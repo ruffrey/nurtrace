33fb4dab7f18d5d639e71f3ce6f786e8634ceaa2
Jeff H. Parrish <jeffhparrish@gmail.com>
2016-12-27 08:05:10 -0800

    wip: working executable tool that can upload and run the exe

--- worker/worker.go
+++ worker/worker.go
@@ -35,15 +35,12 @@ type Worker struct {
 
 // NewWorker is a worker constructor
 func NewWorker(hostAndPort string) (w *Worker, err error) {
-	// u, err := user.Current()
-	// if err != nil {
-	// 	return w, err
-	// }
+	u := os.Getenv("USER")
 	w = &Worker{host: hostAndPort}
 
 	// Dial your ssh server.
 	w.conn, err = ssh.Dial("tcp", w.host, &ssh.ClientConfig{
-		User: "root",
+		User: u,
 		Auth: []ssh.AuthMethod{
 			publicKeyFile(os.Getenv("HOME") + "/.ssh/id_rsa"),
 		},
@@ -114,39 +111,91 @@ func (w *Worker) Train(network *potential.Network, samples []potential.TrainingS
 }
 
 func main() {
-	hostPort := os.Args[1]
-	setup := len(os.Args) > 2 && os.Args[2] == "setup"
-	fmt.Println("New worker at", hostPort)
-	w, err := NewWorker(hostPort)
-	if err != nil {
-		panic(err)
-	}
-	defer w.conn.Close()
-	fmt.Println("Connected to", hostPort)
+	var hostPort string
+	var actionArg string
+	var setup bool
+	var train bool
+	var run bool
 
-	if setup {
-		fmt.Println("Setting up")
-		err = w.transerExecutable()
-		if err != nil {
-			panic(err)
+	if len(os.Args) > 1 {
+		if len(os.Args) > 2 {
+			hostPort = os.Args[1]
+			actionArg = os.Args[2]
+		} else {
+			actionArg = os.Args[1]
+		}
+		switch actionArg {
+		case "setup": // setup remote worker
+			setup = true
+			break
+		case "train": // train remote worker
+			train = true
+			break
+		case "run": // while on remote worker, run the training
+			run = true
+		default:
+			fmt.Println("Last argument should be either 'setup' or 'train'")
+			return
 		}
 	}
 
-	fmt.Println("Training")
+	if setup {
+		fmt.Println("New worker at", hostPort)
+		w, err := NewWorker(hostPort)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer w.conn.Close()
+		fmt.Println("Connected to", hostPort)
 
-	session, err := w.conn.NewSession()
-	if err != nil {
-		panic(err)
-	}
-	defer session.Close()
-	bytes, err := session.CombinedOutput(workerLocation)
-	fmt.Println(string(bytes))
-	if err != nil {
-		fmt.Print(hostPort, " ")
-		panic(err)
+		fmt.Println("Setting up", hostPort)
+		err = w.transerExecutable()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println("Added executable to", hostPort, workerLocation)
+		return
 	}
 
-	if err != nil {
-		panic(err)
+	if train {
+		fmt.Println("New worker at", hostPort)
+		w, err := NewWorker(hostPort)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer w.conn.Close()
+		fmt.Println("Connected to", hostPort)
+
+		fmt.Println("Training", hostPort)
+
+		session, err := w.conn.NewSession()
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer session.Close()
+		bytes, err := session.CombinedOutput(workerLocation + " run")
+		fmt.Println(string(bytes))
+		if err != nil {
+			fmt.Print(hostPort, " ")
+			fmt.Println(err)
+			return
+		}
+		return
 	}
+
+	if run {
+		fmt.Println("Hello World.")
+		return
+	}
+
+	fmt.Println("Usage: [USER=someone] worker [hostname:port] action")
+	fmt.Println("  For running a training session on a remote machine, or running locally.")
+	fmt.Println("  - USER - optional; is an alternative user to use when connecting via ssh")
+	fmt.Println("  - hostname:port - required for setup or train on remote server. bleh.example.com:22")
+	fmt.Println("  - action - [run|setup|train]")
+	fmt.Println("       - run - run training locally with the supplied settings")
+	fmt.Println("       - setup - setup the remote server for training")
+	fmt.Println("       - train - run the training on the remote server")
 }
