9ea6564a7e4ff7cb98e4dc4f5ac8929fe9db2699
Jeff Parrish <jeffhparrish@gmail.com>
2017-03-17 20:16:02 -0700

    simd example

---
+++ simd/simd.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/bjwbell/gensimd/simd"
+)
+
+func main() {
+	a := simd.I16x8{1, 2, 3, 4, 5, 6, 7, 8}
+	b := simd.I16x8{-8, -7, -6, -5, -4, -3, -2, -1}
+
+	result := simd.AddI16x8(a, b)
+	fmt.Println(result)
+}
