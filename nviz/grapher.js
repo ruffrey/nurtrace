9d3f2d1b64c43b9a70b39c9cf22ce5ba39ae961c
Jeff Parrish <jeffhparrish@gmail.com>
2017-03-31 21:58:23 -0700

    Add command buttons to electron app viz

--- nviz/grapher.js
+++ nviz/grapher.js
@@ -6,35 +6,35 @@ require('./forceAtlas2/supervisor');
 require('sigma/plugins/sigma.plugins.animate/sigma.plugins.animate.js');
 require('sigma/plugins/sigma.layout.noverlap/sigma.layout.noverlap.js');
 
-const gunzip = require('zlib').gunzipSync
+const gunzip = require('zlib').gunzipSync;
 const fs = require('fs');
 
 let NETWORK_FILE = process.env.NETWORK_FILE;
 
 while (!NETWORK_FILE) {
-    NETWORK_FILE = electron.dialog.showOpenDialog({
-        properties: ['openFile'],
-        filters: [{
-                name: 'Compressed Network',
-                extensions: ['nur']
-            },
-            {
-                name: 'JSON Network',
-                extensions: ['json']
-            },
-            {
-                name: 'All Files',
-                extensions: ['*']
-            }
-        ]
-    })[0];
+  NETWORK_FILE = electron.dialog.showOpenDialog({
+    properties: ['openFile'],
+    filters: [{
+      name: 'Compressed Network',
+      extensions: ['nur'],
+    },
+    {
+      name: 'JSON Network',
+      extensions: ['json'],
+    },
+    {
+      name: 'All Files',
+      extensions: ['*'],
+    },
+    ],
+  })[0];
 }
 
 const rawText = gunzip(fs.readFileSync(NETWORK_FILE));
 const nw = JSON.parse(rawText.toString('utf8'));
 const g = {
-    nodes: [],
-    edges: []
+  nodes: [],
+  edges: [],
 };
 const green = '#00FF2D';
 const blue = '#0775FF';
@@ -46,48 +46,48 @@ const isInput = tag => tag && tag.substring(0, 3) === 'in-';
 const isMiddle = tag => !tag;
 
 Object.keys(nw.Cells).forEach((cellId, i) => {
-    const cell = nw.Cells[cellId];
+  const cell = nw.Cells[cellId];
 
-    let x;
-    let y;
+  let x;
+  let y;
 
-    if (isInput(cell.Tag)) {
-        layerInput++;
-        x = layerInput;
-        y = 200;
-    } else if (isMiddle(cell.Tag)) {
-        layerMiddle++;
-        x = layerMiddle;
-        y = 100;
-    } else {
-        layerOutput++;
-        x = layerOutput;
-        y = 0;
-    }
+  if (isInput(cell.Tag)) {
+    layerInput++;
+    x = layerInput;
+    y = 200;
+  } else if (isMiddle(cell.Tag)) {
+    layerMiddle++;
+    x = layerMiddle;
+    y = 100;
+  } else {
+    layerOutput++;
+    x = layerOutput;
+    y = 0;
+  }
 
     // x = 100 * Math.cos(2 * i * Math.PI / N); // random location
     // y = 100 * Math.sin(2 * i * Math.PI / N); // random location
 
-    g.nodes.push({
-        id: cell.ID,
-        label: cell.Tag || cell.ID,
-        size: Object.keys(cell.AxonSynapses).length + Object.keys(cell.DendriteSynapses).length,
-        color: cell.Tag ?
+  g.nodes.push({
+    id: cell.ID,
+    label: cell.Tag || cell.ID,
+    size: Object.keys(cell.AxonSynapses).length + Object.keys(cell.DendriteSynapses).length,
+    color: cell.Tag ?
             isInput(cell.Tag) ? blue : green
             // : '#' + (Math.floor(Math.random() * 16777215).toString(16) + '000000').substr(0, 6),
             :
             '#cccccc',
-        x,
-        y
-    });
+    x,
+    y,
+  });
 });
-Object.keys(nw.Synapses).forEach(synapseId => {
-    const synapse = nw.Synapses[synapseId];
-    g.edges.push({
-        id: synapse.ID,
-        source: synapse.FromNeuronAxon,
-        target: synapse.ToNeuronDendrite
-    });
+Object.keys(nw.Synapses).forEach((synapseId) => {
+  const synapse = nw.Synapses[synapseId];
+  g.edges.push({
+    id: synapse.ID,
+    source: synapse.FromNeuronAxon,
+    target: synapse.ToNeuronDendrite,
+  });
 });
 
 // let i,
@@ -136,36 +136,40 @@ Object.keys(nw.Synapses).forEach(synapseId => {
 //     }
 // }
 
-var s = new sigma({
-    graph: g,
-    container: 'graph-container',
-    settings: {
-        drawEdges: true,
+const s = new sigma({
+  graph: g,
+  container: 'graph-container',
+  settings: {
+    drawEdges: true,
 
-        minNodeSize: 2,
-        maxNodeSize: 10,
-        minEdgeSize: 1,
-        maxEdgeSize: 10,
-    }
+    minNodeSize: 2,
+    maxNodeSize: 10,
+    minEdgeSize: 1,
+    maxEdgeSize: 10,
+  },
 });
 
-// Start the ForceAtlas2 algorithm:
-s.startForceAtlas2({
+
+window.start = () => {
+  s.startForceAtlas2({
     worker: false,
-    // linLogMode: false,
+        // linLogMode: false,
     outboundAttractionDistribution: true,
     adjustSizes: true,
-    // edgeWeightInfluence: 0,
-    // scalingRatio: 1,
-    // strongGravityMode: false,
-    // gravity: 1,
-    // slowDown: 1,
-    // barnesHutOptimize: false,
-    // barnesHutTheta: 0.5,
-    // startingIterations: 1,
-    // iterationsPerRender: 1
-});
+        // edgeWeightInfluence: 0,
+        // scalingRatio: 1,
+        // strongGravityMode: false,
+        // gravity: 1,
+        // slowDown: 1,
+        // barnesHutOptimize: false,
+        // barnesHutTheta: 0.5,
+        // startingIterations: 1,
+        // iterationsPerRender: 1
+  });
+};
 
-// setTimeout(() => {
-// s.stopForceAtlas2();
-// }, 10000);
+window.start();
+
+window.stop = () => {
+  s.stopForceAtlas2();
+};
